1645734a9fcaa2b288bd37df7768d4df48f3c4ff
maximoivk78 <loswawas818@gmail.com>
2025-04-23 08:30:00 -0300

    Add files via upload

---
+++ ejercicio 1 tk.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+def main ():
+    
+    ventana = tk.Tk()
+    ventana.title("hola")
+    ventana.geometry ("500x400")
+    ventana.configure (bg="blue")
+
+
+   
+ 
+if __name__=="__main__":
+    main () 
